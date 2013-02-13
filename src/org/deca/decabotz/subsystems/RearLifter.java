f7e8a6e6950f46f98a8b9a12c9ed5d8353410d34
AymanBazar <abazar@daytonearlycollege.org>
2013-02-13 17:30:23 -0500

    Started work on air rear lifter

--- src/org/deca/decabotz/subsystems/RearLifter.java
+++ src/org/deca/decabotz/subsystems/RearLifter.java
@@ -4,36 +4,28 @@
  */
 package org.deca.decabotz.subsystems;
 
-import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.can.CANTimeoutException;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.deca.decabotz.RobotMap;
 
 /**
  *
  * @author Sciencelab
  */
 public class RearLifter extends Subsystem {
+    private Solenoid lift;
+    private Solenoid retract;
+    
+    public RearLifter() {
+        lift = new Solenoid(channel);
+    retract = new Solenoid (channel);
+    }
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    CANJaguar rearLiftJag;
-    public RearLifter() throws CANTimeoutException {
-        try {
-            rearLiftJag = new CANJaguar(RobotMap.rearLiftJagID);
-        } catch (CANTimeoutException ex) {
-            throw ex;
-        }
-    }
+   
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
         setDefaultCommand(new org.deca.decabotz.commands.RearLift());
     }
-    public void setRearLifter(double speedValue) {
-        try {
-            rearLiftJag.setX(speedValue);
-        } catch (CANTimeoutException ex) {
-            ex.printStackTrace();
-        }
-    }
+   
 }
